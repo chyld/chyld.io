4dc2aefbc28f19922d00c5f64da93bad8b6ff1c4
Chyld Medford <chyld.medford@gmail.com>
2013-07-10 01:04:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChyldIo::Application.config.secret_key_base = 'ec8ee82396be71a0f79ab75095679c6c98f930d67ef37cf6f7ff3b36fc93134e71178e20d3ee1185c1d07dfd9a15037062306c80909b6d8bf59bdf212cb58d37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChyldIo::Application.config.session_store :cookie_store, key: '_chyld_io_session'
